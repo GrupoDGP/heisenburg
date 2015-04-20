85375a492264d457aa6d762039bd11701c4910b4
softwarejimenez <softwarejimenezz@gmial.com>
2015-04-20 12:31:09 +0200

    transito php

---
+++ php/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div>
+        <div id="footer_Superior_Derecho">
+            <img src="./img/logo_etsiit.png"/>
+        </div>
+
+        <div id="footer_Superior_Izquierdo">
+            <p>Dirección:Calle Periodista Daniel Saucedo Aranda s/n </p>
+            <p>Teléfono: +34-958242802</p>
+            <p>Email: informacion@heisenburg.es </p>
+        </div>
+    </div>
+    <div id="footer_Inferior">
+        <p>&copy; 2015 Heisenburg.es<p>
+    </div>
+</footer> <!-- end footer -->
