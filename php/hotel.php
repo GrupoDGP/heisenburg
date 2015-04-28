d3255ef4c8414746cd9e827884006cf32958a040
demiurgosoft <demiurgosoft@hotmail.com>
2015-04-28 17:34:03 +0200

    database handler & hotel class

--- php/hotel.php
+++ php/hotel.php
@@ -15,6 +15,39 @@ class Hotel{
     var $servicios;
     var $imagenes;
 
+    //se le pasa una "row" del query
+    function read_hotel($row){
+        this->$nombre=$row["nombre"];
+        this->$tipo=$row["tipo"];
+        this->$precio=$row["precio"];
+        this->$calle=$row["calle"];
+        this->$estrellas=$row["estrellas"]; //0-5
+        this->$ciudad=$row["ciudad"];
+        this->$telefono=$row["tlfn"];
+        this->$email=$row["email"];
+        this->$video=$row["video"];
+        this->$resumen=$row["resumen"];
+        this->$servicios=$row["servicios"];
+        this->$imagenes=$row["imagenes"];
+    }
+
+}
+//devuelve un array de los hoteles de dicha query
+function leer_hoteles($query,$dbhandler){
+$dbhandler->connect();
+$table=$dbhandler->query($query);
+$result=array();
+    if ($table->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $hotel=new Hotel;
+            $hotel->read_hotel($row);
+            $result[]=$hotel;
+        }
+    } else {
+        echo "no results";
+$dbhandler->close();
+return $result;
 }
 
 
