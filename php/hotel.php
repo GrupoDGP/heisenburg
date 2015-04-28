10a463589fa35d23d71a8ea84963d179601f4ae4
demiurgosoft <demiurgosoft@hotmail.com>
2015-04-28 17:07:57 +0200

    clase hotel

---
+++ php/hotel.php
@@ -0,0 +1,24 @@
+<?php
+//Clase Hotel
+
+class Hotel{
+    var $nombre;
+    var $tipo; //string hotel o casa
+    var $precio;
+    var $calle;
+    var $estrellas; //0-5
+    var $ciudad;
+    var $telefono;
+    var $email;
+    var $video;
+    var $resumen;
+    var $servicios;
+    var $imagenes;
+
+}
+
+
+
+
+
+?>
