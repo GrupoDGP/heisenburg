648a26a369f204ca0fc725d0f85e40fbb96f4fa3
root <root@Baldomero>
2015-05-19 17:43:08 +0200

    working example

--- rest/api.php
+++ rest/api.php
@@ -1,38 +1,32 @@
 <?php
-//Use httprequest somehow
 class api_rest{
 public function process_api(){	
-	echo "processing api</br>";
+
 		$func = strtolower(trim(str_replace("/","",$_REQUEST['rquest'])));
 			if((int)method_exists($this,$func) > 0){
 				$this->$func();
 			}else{
-			//	echo "page not found</br>";
-				$this->response('',404); // Page not Found
+				http_response_code(404);
 				}
 	}
 	private function hoteles(){
-	echo "sending hotels";
-		if($this->get_request_method() != "GET"){
-		$this->response('',406);
+		if($_SERVER['REQUEST_METHOD']!="GET"){
+			http_response_code(406);
 		}
-	$respuesta= array("status" => "Success","msg" => "Hotel Super Chulo");
-	$this->response($this->json($respuesta),200);
-	//$id = (int)$this->_request['id']; // _request es rellenado previamente
-	if($id > 0) {
-		mysql_query("DELETE FROM users WHERE user_id = $id");
-		$success = array("status" => "Success","msg" => "Successfully one record deleted.");
-		$this->response($this->json($success),200);
-	}
-	else {
-		$this->response('',204); // If no records "No Content" status
-	}
+		else{
+			if(!isset($_REQUEST['tipo']) || !isset($_REQUEST['finicio']) || !isset($_REQUEST['ffin'])){
+				http_response_code(204);
+			}
+			else{
+				$tipo = strtolower(trim(str_replace("/","",$_REQUEST['tipo'])));
+				$fechainicio = strtolower(trim(str_replace("/","",$_REQUEST['finicio'])));
+				$fechafin = strtolower(trim(str_replace("/","",$_REQUEST['ffin'])));
+				http_response_code(200);
+				$response= array("tipo" => $tipo, "fechainicio"=> $fechainicio, "fechafin"=>$fechafin);
+				echo json_encode($response);
+			}
 	
-	exit(json_encode($arr));	
-	echo "what</br>";
-	//exit(json_encode($respuesta));
-	//echo json_encode($respuesta);
-	//$this->response($this->json($respuesta),200);
+		}	
 	}
 
 }
