30350273eb3e63cec71bb60007bc9560d034e50b
root <root@Baldomero>
2015-05-20 20:40:42 +0200

    api rest (php)

--- rest/api_rest_example/serv/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//$num1 = $_REQUEST['num1'];
-//$num2 = $_REQUEST['num2'];
-
-//$total = $num1 + $num2;
-$respuesta= array("status" => $_SERVER['REQUEST_METHOD'],"msg" => "Hotel Super Chulo");
-echo json_encode($respuesta);
-?>
