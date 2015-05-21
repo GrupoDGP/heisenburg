fe0c63d8d9f3f739d39f67581aded787e567f003
sofwarejimenez <sofwarejimenezz@gmail.com>
2015-05-21 16:13:42 +0200

    añadir habitacion

--- php/addhabitacion.php
+++ php/addhabitacion.php
@@ -2,27 +2,28 @@
 	<h1> PROCEDIMIENTO AGREGAR HABITACION </h1>
 </div>
 <?php
-					
+
 	$dbhandler = new db_handler("localhost","root","heisenburg");
-	$tipo="select nombre from `alojamientos`";
+	$tipo="select idAlojamiento, nombre from `alojamientos`";
 	$dbhandler->connect();
 	$consulta=$dbhandler->query($tipo);
 ?>
 
 <div id="formularioContacto">
-    <form method="post" action="index.php?page=addhotel" >
+    <form method="post" action="index.php?page=addhabitacion" >
         <table id="tablaformulario">
             <tr>
                 <td> <label> Nº de camas: </label> </td>
-                <td> <input type="text" name="camas"  placeholder="Introduce # de camas"  required="true" autofocus> </input> </td>
-				
+                <td> <input type="number" name="camas"  placeholder="Introduce # de camas"  required="true" autofocus> </input> </td>
+
                 <td><label> Nombre Alojamiento: </label> </td>
-                <td><select name="alojamiento"> 	
+                <td><select name="alojamiento">
                     <?php
-					
-						while( $res = $consulta->fetch_assoc() )
-							echo '<option value="hotel">'.$res['nombre'].'</option>';
-					
+
+						while( $res = $consulta->fetch_assoc() ){
+							echo "<option value=\"".$res["idAlojamiento"]."\">".$res["nombre"]."</option>";
+						}
+
 					?>
                 </td>
             </tr>
@@ -30,7 +31,7 @@
                 <td><label > Precio: </label> </td>
                 <td><input type="number" name="precio"  placeholder="precio minimo." required="true" > </input> </td>
             </tr>
-            
+
             <tr>
                 <td><button type="submit" name="submit">Enviar</button></td>
             </tr>
@@ -41,11 +42,10 @@
 
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        include "php/includes/dbhandler.php";
         $dbhandler = new db_handler("localhost","root","heisenburg");
         $dbhandler->connect();
-        $sql="insert into habitacion(camas,precio, alojamiento) values ('$_REQUEST[camas]','$_REQUEST[precio]','$_REQUEST[alojamiento]')";
+        $sql="insert into habitacion(tipo_hab,precio, idAlojamiento) values ('".$_REQUEST["camas"]."','".$_REQUEST["precio"]."','".$_REQUEST["alojamiento"]."')";
+
         if ($dbhandler->query($sql) === TRUE) {
             echo "<script> alert(\"Habitacion dada de alta\");</script>";
         } else {
